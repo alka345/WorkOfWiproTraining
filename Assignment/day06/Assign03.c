0764623c5047781770d03c01b92201f14f46754b
alka345 <alkaraojnv@gmail.com>
2024-04-22 06:41:15 +0530

    Equilibrium first occur

---
+++ Assignment/day06/Assign03.c
@@ -0,0 +1,28 @@
+/*
+Given an array A of N positive numbers. The task is to find the position
+where equilibrium first occurs in the array. Equilibrium position in an array is a position such that the sum of elements before it is equal to the sum of   elements after it.
+
+Input Format:
+The first line contains an integer, denoting the size of the array.
+The second line contains integers denoting the elements
+of the array.
+Constraints
+1<= n<=100
+1<=arr<= 1000, where arr is the ith element of the array.
+
+Output Format:
+The output line contains integer denoting an equilibrium
+index (if any) or -1 (if no equilibrium indexes exist).
+TESTCASE 1:
+Input:
+7
+[-7, 1, 5, 2, -4, 3, 1]
+3
+[-7, 0, 0, 0, -4, 4, 0]
+0
+[0 0 0 0 -1]
+4
+Output:
+3
+
+*/
